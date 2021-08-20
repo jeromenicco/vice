02ad188813900816327f15949761a5d8ae99d57e
Jeromenuballers <79321289+Jeromenuballers@users.noreply.github.com>
2021-08-20 12:13:02 +0200

    responsive V0.1

--- src/components/RadioMenu.js
+++ src/components/RadioMenu.js
@@ -1,32 +1,29 @@
-import React from 'react'
-import { radioList } from '../data'
-import { Link } from 'react-router-dom'
-import './RadioMenu.css'
+import React from "react";
+import { radioList } from "../data";
+import { Link } from "react-router-dom";
+import "./RadioMenu.css";
+import { useMediaQuery } from "react-responsive";
 
 function RadioMenu({ visible }) {
+  // const isMobileDevice = useMediaQuery({
+  //   query: "(min-device-width: 375px)",
+  // });
+
+  // console.log(isMobileDevice === true);
 
   return (
-    <div className='menu-container'>
-      <ul
-        className={
-          visible
-          ? 'menu-items-visible'
-          : 'menu-items-hidden'
-        }
-      >
-        
-        {
-          radioList.map((item, index) => 
-            <li key={index}>
-              <Link to={item.path} >
-                <p className='menu-items'>{item.name}</p>
-              </Link>
-            </li>
-          )
-        }
+    <div className="menu-container">
+      <ul className={visible ? "menu-items-visible" : "menu-items-hidden"}>
+        {radioList.map((item, index) => (
+          <li key={index}>
+            <Link to={item.path}>
+              <p className="menu-items">{item.name}</p>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default RadioMenu
+export default RadioMenu;
