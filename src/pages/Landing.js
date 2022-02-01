0463eb72652230760d859d182c9f0e48a879cbea
Jerome Nicco <jeromenicco@macbook-air-de-jerome.home>
2022-02-01 19:18:47 +0100

    landing wip

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import viceLogo from "../assets/vice_main_logo.png";
+import { useSpring, animated } from 'react-spring'
 
 
 import './Landing.css'
 
 function Landing() {
-    // const [landing, setLanding] = useState(false)
+
+    const fadeIn = useSpring({
+        to: { opacity: 1 },
+        from: { opacity: 0 },
+        config: { duration: 2000 }
+      })
 
     const handleLanding = () => {
         history.push('/flash')
-        // setLanding(true)
     }
 
     const history = useHistory()
     return (
         <div className='landing-container'>
-            <img className="vice-logo" src={`${viceLogo}`} alt="main-logo" />
-            <p>Get wasted on music</p>
-            <button onClick={handleLanding}>Play</button>
+            <animated.div style={{...fadeIn}} className='landing-container'>
+                <img className="vice-logo" src={`${viceLogo}`} alt="main-logo" />
+                <p>Get wasted on music</p>
+                <button onClick={handleLanding}>Play</button>
+            </animated.div>
         </div>
     )
 }
